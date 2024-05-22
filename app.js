3401e1d9e021095021091e62d267b6cfc11f5cfd
Madelyn Sadler <sadlerm@oregonstate.edu>
2024-05-21 18:32:46 -0700

    Setup handlebars and connect to database

--- app.js
+++ app.js
@@ -3,17 +3,31 @@
 /*
     SETUP
 */
+// Database
+var db = require('./database/db-connector')
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 1749;                 // Set a port number at the top so it's easy to change in the future
 
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
-    });                                         // requesting the web site.
+
+app.get('/', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Classes;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query                                        // will process this file, before sending the finished HTML to the client.
 
 /*
     LISTENER
