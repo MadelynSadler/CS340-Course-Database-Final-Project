a7dc11337e65775569b0795aa1c3f30f9d5940ab
Madelyn Sadler <sadlerm@oregonstate.edu>
2024-05-23 16:54:26 -0700

    Update according to step 5 (doesn't work)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 PORT        = 1749;                 // Set a port number at the top so it's easy to change in the future
 
 var db = require('./database/db-connector');
@@ -27,6 +30,59 @@ app.get('/', function(req, res)
             res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
     });                                                         // received back from the query                                      // requesting the web site.
+
+app.post('/add-session-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Capture NULL values
+        let classNumber = parseInt(data.classNumber);
+        if (isNaN(classNumber))
+        {
+            classNumber = 'NULL'
+        }
+
+        let week = parseInt(data.week);
+        if (isNaN(week))
+        {
+            week = 'NULL'
+        }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO sessions (classNumber, day, week, topic) VALUES ('${data.classNumber}', '${data.day}', ${week}, ${topic})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on sessions
+                query2 = `SELECT * FROM Sessions;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
 
 /*
     LISTENER
---
+++ public/js/add_session.js
@@ -0,0 +1,97 @@
+// Get the objects we need to modify
+let addPersonForm = document.getElementById('add-session-form-ajax');
+
+// Modify the objects we need
+addPersonForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let classNumber = document.getElementById("input-classNumber");
+    let day = document.getElementById("input-day");
+    let week = document.getElementById("input-week");
+    let topic = document.getElementById("input-topic");
+
+    // Get the values from the form fields
+    let classNumberValue = classNumber.value;
+    let dayValue = day.value;
+    let weekValue = week.value;
+    let topicValue = topic.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        classNumber: classNumberValue,
+        day: dayValue,
+        week: weekValue,
+        topic: topicValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-session-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            classNumber.value = '';
+            day.value = '';
+            week.value = '';
+            topic.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Sessions
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("sessions-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let sessionIDCell = document.createElement("TD");
+    let classNumberCell = document.createElement("TD");
+    let dayCell = document.createElement("TD");
+    let weekCell = document.createElement("TD");
+    let topicCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    sessionIDCell.innerText = newRow.sessionID;
+    classNumberCell.innerText = newRow.classNumber;
+    dayCell.innerText = newRow.day;
+    weekCell.innerText = newRow.week;
+    topicCell.innerText = newRow.topic;
+
+    // Add the cells to the row 
+    row.appendChild(sessionIDCell);
+    row.appendChild(classNumberCell);
+    row.appendChild(dayCell);
+    row.appendChild(weekCell);
+    row.appendChild(topicCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
