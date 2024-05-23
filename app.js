2c14786905acc5501cf03910c60adf68dd05a26b
Madelyn Sadler <sadlerm@oregonstate.edu>
2024-05-23 13:26:03 -0700

    Step 3 working

--- app.js
+++ app.js
@@ -3,31 +3,23 @@
 /*
     SETUP
 */
-// Database
-var db = require('./database/db-connector')
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 1749;                 // Set a port number at the top so it's easy to change in the future
 
+// Handlebars
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-
 /*
     ROUTES
 */
-
-app.get('/', function(req, res)
-    {  
-        let query1 = "SELECT * FROM Classes;";               // Define our query
-
-        db.pool.query(query1, function(error, rows, fields){    // Execute the query
-
-            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
-        })                                                      // an object where 'data' is equal to the 'rows' we
-    });                                                         // received back from the query                                        // will process this file, before sending the finished HTML to the client.
+app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
+    {
+        res.render('index');
+    });                                         // requesting the web site.
 
 /*
     LISTENER
